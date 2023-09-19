07683ad730108b479eb7c60f4619db49c18f5fe3
Iso-Doss <dosou.israel48@gmail.com>
2023-09-19 14:45:43 +0100

    Ajout d'une propriété profile avec une valeur par defaut a customer et un constructeur au controlleur de base

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected string $profile = 'customer';
+
+    public function __construct(string $profile = 'customer')
+    {
+        $this->profile = $profile;
+        View::share('profile', $this->profile);
+    }
 }
