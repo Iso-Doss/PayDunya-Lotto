56c162eb36e6019003aed32244fd55b3674c1a23
Iso-Doss <dosou.israel48@gmail.com>
2023-09-19 15:24:02 +0100

    Ajout de la classe qui permettra de gerer l'envoi des mails

---
+++ app/Mail/UserAccountMail.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class UserAccountMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public User $user, public array $data)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            to: (isset($this->data['email']) && !empty($this->data['email'])) ? $this->data['email'] : $this->user->email,
+            subject: $this->data['title']
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: $this->data['view']
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
