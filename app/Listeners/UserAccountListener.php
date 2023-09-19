2c8ea4acdef62ad69527e67e4c34a62337d24e16
Iso-Doss <dosou.israel48@gmail.com>
2023-09-19 15:22:34 +0100

    Ajout d'écouteur d'evennement pour la gestion de compte utilisateur

---
+++ app/Listeners/UserAccountListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserAccountEvent;
+use App\Notifications\UserAccountNotification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailer;
+
+//use Illuminate\Contracts\Queue\ShouldQueue;
+//use Illuminate\Queue\InteractsWithQueue;
+
+class UserAccountListener implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct(private readonly Mailer $mailer)
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(UserAccountEvent $event): void
+    {
+        $event->user->notify(new UserAccountNotification($event->user, $event->data));
+    }
+}
