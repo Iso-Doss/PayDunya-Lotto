9ec768ebfc7d4e0a5b55515b3d67d835273b6fab
Iso-Doss <dosou.israel48@gmail.com>
2023-09-19 15:22:06 +0100

    Ajout de l'evennement pour la gestion de compte utilisateur

---
+++ app/Events/UserAccountEvent.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+//use Illuminate\Broadcasting\Channel;
+//use Illuminate\Broadcasting\PresenceChannel;
+//use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class UserAccountEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public User $user, public array $data = [])
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
