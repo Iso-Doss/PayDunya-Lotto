bbcccabff656e0a8c9d10131cbdbab892a359be7
Iso-Doss <dosou.israel48@gmail.com>
2023-09-19 15:31:44 +0100

    Ajout du model PasswordResetToken

---
+++ app/Models/PasswordResetTokens.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @mixin IdeHelperPasswordResetTokens
+ */
+class PasswordResetTokens extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'email',
+        'new_email',
+        'profile',
+        'type',
+        'token',
+    ];
+}
