51b25462af3e8a24aad891e271c9ee53b86482a9
Iso-Doss <dosou.israel48@gmail.com>
2023-09-21 12:00:35 +0100

    Mise en place du controller d'authentification du client

---
+++ app/Http/Controllers/Customer/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+
+    /**
+     * Auth controller construct.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
