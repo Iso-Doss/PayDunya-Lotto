fe3d8d393987fc391a64408ba395ec89b67ef4b1
Iso-Doss <dosou.israel48@gmail.com>
2023-09-29 15:00:40 +0100

    Mise a jour du middleware qui permet de rediriger l'utilisateur s'il est deja connecter

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,16 +12,29 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param Request $request
+     * @param Closure(Request): (Response) $next
+     * @param string ...$guards
+     * @return Response
      */
     public function handle(Request $request, Closure $next, string ...$guards): Response
     {
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+
+            if ($guard == "customer" && Auth::guard($guard)->check()) {
+                return redirect('/dashboard');
             }
+
+            if ($guard == "administrator" && Auth::guard($guard)->check()) {
+                return redirect('/admin/dashboard');
+            }
+
+            //if (Auth::guard($guard)->check()) {
+            //    return redirect(RouteServiceProvider::HOME);
+            //}
+
         }
 
         return $next($request);
