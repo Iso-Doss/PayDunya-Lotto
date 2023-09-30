0fa6f7afbe43be4f897073ac64c7d3dd9468ae6f
Iso-Doss <dosou.israel48@gmail.com>
2023-09-30 16:48:32 +0100

    Mise a jour du controller de bas en y ajoutant les fonction upload d'image et getGuard

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\View;
 
@@ -17,5 +19,40 @@ class Controller extends BaseController
     {
         $this->profile = $profile;
         View::share('profile', $this->profile);
+    }
+
+    /**
+     * Upload image.
+     *
+     * @param Request $request The request.
+     * @param string $path The path.
+     * @return bool|string The upload image.
+     */
+    public function uploadImage(Request $request, string $path): bool|string
+    {
+        /** @var UploadedFile | null $image */
+        $image = $request->validated('image');
+        if (null === $image || $image->getError()) {
+            return false;
+        }
+        return $image->store($path, 'public');
+    }
+
+    /**
+     * Get the profile guard.
+     *
+     * @param string $profile The profile.
+     * @return string $guard The guard.
+     */
+    public function getGuard(string $profile): string
+    {
+        $guard = 'customer';
+        if ('AGENT' == $profile) {
+            $guard = 'agent';
+        } else if ('ADMINISTRATOR' == $profile) {
+            $guard = 'administrator';
+        }
+
+        return $guard;
     }
 }
